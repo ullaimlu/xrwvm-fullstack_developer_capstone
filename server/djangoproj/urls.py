367806ac8421eb690eb92eb2026722d47116d5e0
ullaimlu <marianac1707m@gmail.com>
2024-07-15 15:38:33 -0400

    Dealers frontend added

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +28,6 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+    re_path('dealers/', TemplateView.as_view(template_name="index.html")),
+    re_path('dealer/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
